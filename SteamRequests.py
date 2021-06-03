4a6e2d2e7c98d24863b70429cb584d7e7670ffb7
Mazawrath <Mazawrath@users.noreply.github.com>
2021-06-02 23:17:07 -0400

    Polishing up file

--- SteamRequests.py
+++ SteamRequests.py
@@ -8,26 +8,22 @@ key = argv[1]
 
 def get_steam_id(vanity_url):
     url = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/"
-    # defining a params dict for the parameters to be sent to the API
     params = {'key': key, "vanityurl": vanity_url}
-    # sending get request and saving the response as response object
     r = requests.get(url=url, params=params)
     data = r.json()
     return data['response']['steamid']
 
 
 def get_games_owned(steam_id):
-    retVar = []
-    # api-endpoint
+    ret_var = []
     url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/"
-    # defining a params dict for the parameters to be sent to the API
     params = {'key': key, "steamid": steam_id, 'include_appinfo': False,
               'include_played_free_games': True, }
     r = requests.get(url=url, params=params)
     data = r.json()
     for game_data in data['response']['games']:
-        retVar.append(game_data['appid'])
-    return retVar
+        ret_var.append(game_data['appid'])
+    return ret_var
 
 
 def get_related_games(steam_ids, matching_requirement):
@@ -38,12 +34,6 @@ def get_related_games(steam_ids, matching_requirement):
         player_games.append(get_games_owned(steam_ids))
     for i in range(len(player_games)):
         for game in player_games[i]:
-            # if game not in player_games[i]:
-            #     player_games[0].remove(game)
-            # else:
-            #     matching_games.append(game)
-
-            # if game in player_games[i]:
             if game not in matching_games:
                 matching_games.append(game)
                 matching_games_count.append(1)
@@ -52,8 +42,7 @@ def get_related_games(steam_ids, matching_requirement):
                 matching_games_count[location] += 1
 
     idx = 0
-    # for idx, count in enumerate(matching_games_count):
-    while (idx < len(matching_games_count)):
+    while idx < len(matching_games_count):
         if matching_games_count[idx] < matching_requirement:
             matching_games.pop(idx)
             matching_games_count.pop(idx)
@@ -66,9 +55,7 @@ def get_related_games(steam_ids, matching_requirement):
 
 def get_games_info(steam_ids, game_ids):
     game_infos = []
-    # for game in game_ids:
     url = 'https://store.steampowered.com/api/appdetails?'
-    # defining a params dict for the parameters to be sent to the API
     game_list = ""
     for game in game_ids:
         game_list += str(game) + ','
@@ -89,7 +76,6 @@ def get_games_info(steam_ids, game_ids):
 def update_all_games():
     url = "http://api.steampowered.com/ISteamApps/GetAppList/v0002"
     params = {'key': key}
-    # sending get request and saving the response as response object
     r = requests.get(url=url, params=params)
     data = r.json()
     print(json.dumps(data))
