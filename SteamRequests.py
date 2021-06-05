c057333b93790284890ea7531ae60275b89ea34a
Mazawrath <Mazawrath@users.noreply.github.com>
2021-06-05 19:18:22 -0400

    Put updating database into its own method and polished up updating every game

--- SteamRequests.py
+++ SteamRequests.py
@@ -100,45 +100,69 @@ def get_game_info(game_id):
         return None
 
 
+def __update_database__(game_info):
+    categories = []
+    if 'categories' in game_info:
+        for category in game_info['categories']:
+            categories.append(category['description'])
+    else:
+        print("Categories not found!\n" + json.dumps(game_info))
+    genres = []
+    if 'genres' in game_info:
+        for genre in game_info['genres']:
+            genres.append(genre['description'])
+    else:
+        print("Genres not found!\n" + json.dumps(game_info))
+    name = game_info['name'].replace('"', '\'\'')
+    store_link = "https://store.steampowered.com/app/" + str(game_info['steam_appid']) + "/"
+    launch_link = "steam://rungameid/" + str(game_info['steam_appid'])
+    box_art = "https://steamcdn-a.akamaihd.net/steam/apps/" + str(game_info['steam_appid']) + "/library_600x900_2x.jpg"
+    # If a game doesn't have box art, replace it with the next best thing.
+    if requests.get(box_art).status_code != 200:
+        box_art = "https://cdn.akamai.steamstatic.com/steam/apps/" + str(game_info['steam_appid']) + "/header.jpg"
+    Database.update_game(game_info['steam_appid'], name, store_link, launch_link, box_art, ",".join(categories),
+                         ",".join(genres))
+
+
 def update_all_games():
     url = "http://api.steampowered.com/ISteamApps/GetAppList/v0002"
     params = {'key': key}
     r = requests.get(url=url, params=params)
     data = r.json()
+    print(r.status_code)
     # Traverse through every single Steam app
     for game in data['applist']['apps']:
+        print(game)
         # Get detailed info for each app
         game_info = get_game_info(game['appid'])
         # Only add an app if there is actually data in game_info and it is a game
         if game_info and game_info['type'] == 'game':
-            categories = []
-            if 'categories' in game_info:
-                for category in game_info['categories']:
-                    categories.append(category['description'])
-            else:
-                print("Categories not found!\n" + json.dumps(game_info))
-            genres = []
-            if 'genres' in game_info:
-                for genre in game_info['genres']:
-                    genres.append(genre['description'])
-            else:
-                print("Genres not found!\n" + json.dumps(game_info))
-            name = game_info['name'].replace('"', '\'\'')
-            store_link = "https://store.steampowered.com/app/" + str(game['appid']) + "/"
-            launch_link = "steam://rungameid/" + str(game['appid'])
-            box_art = "https://steamcdn-a.akamaihd.net/steam/apps/" + str(game['appid']) + "/library_600x900_2x.jpg"
-            # If a game doesn't have box art, replace it with the next best thing.
-            if requests.get(box_art).status_code != 200:
-                box_art = "https://cdn.akamai.steamstatic.com/steam/apps/" + str(game['appid']) + "/header.jpg"
-            Database.update_game(game['appid'], name, store_link, launch_link, box_art, ",".join(categories),
-                                 ",".join(genres))
+            __update_database__(game_info)
 
 
 def get_matching_games_info(steam_ids, matching_games):
-    ret_val = {}
     games_owned = []
+
+    ret_val = {"categories": [], "games": []}
+
     for steam_id in steam_ids:
         games_owned.append(get_games_owned(steam_id))
     for app_id in matching_games:
         game_info = Database.get_game(app_id)
-        print(game_info)
+        if not game_info:
+            print(str(app_id) + " not found!")
+            continue
+        # Game not found in database, try to find it
+        # if not game_info:
+        ret_info = {
+            "app_id": game_info['app_id'],
+            "name": game_info['name'],
+            "store_link": game_info['store_link'],
+            "launch_link": game_info['launch_link'],
+            "box_art": game_info['box_art'],
+            "categories": game_info['categories'],
+            "genres": game_info['genres']
+        }
+        ret_val["games"].append(ret_info)
+
+    return json.dumps(ret_val)
