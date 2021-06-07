7ed8f31f7f9037965965af95d2b4da9904161056
Mazawrath <Mazawrath@users.noreply.github.com>
2021-06-06 23:57:26 -0400

    Made database store all Steam apps rather then just games

--- SteamRequests.py
+++ SteamRequests.py
@@ -138,8 +138,8 @@ def update_games(update_existing):
             continue
         # Get detailed info for each app
         game_info = get_game_info(game['appid'])
-        # Only add an app if there is actually data in game_info and it is a game or mod
-        if game_info and (game_info['type'] == 'game' or game_info['type'] == 'mod'):
+        # Only add an app if there is actually data in game_info
+        if game_info:
             __update_database__(game_info)
 
 
