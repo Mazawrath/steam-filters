8cf8e052f39f47f900d5494d005ce93aed562a92
Mazawrath <Mazawrath@users.noreply.github.com>
2021-06-10 00:15:12 -0400

    Added basic navbar

--- filter-website/src/App.js
+++ filter-website/src/App.js
@@ -4,7 +4,32 @@ import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">Steam Filters</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+  );
+}
 
 class SteamForm extends React.Component {
   handleSubmit(event) {
@@ -41,11 +66,14 @@ class SteamForm extends React.Component {
 
 function App() {
   return (
+    <div>
+    <NavBar></NavBar>
     <Container className="p-3">
       <Jumbotron>
       <SteamForm></SteamForm>
       </Jumbotron>
     </Container>
+    </div>
   );
 }
 
