bab828978fdfe0ea56001bd14de847568c0b8eed
Mazawrath <Mazawrath@users.noreply.github.com>
2021-06-10 00:05:14 -0400

    Added a basic form along with functionality to detect typing and submitting data

--- filter-website/src/App.js
+++ filter-website/src/App.js
@@ -4,30 +4,34 @@ import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
-import 
+import Form from 'react-bootstrap/Form'
 
 class SteamForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
   handleSubmit(event) {
-    alert('wow, such a cool button!');
+    const formData = new FormData(event.target),
+    formDataObj = Object.fromEntries(formData.entries())
+    console.log(formDataObj)
+
+    alert('wow, such a cool button!' + formDataObj.steamId);
     event.preventDefault();
+  }
+
+  onInput(event) {
+    alert("Cool letter! " +  event.target.value);
   }
 
   render() {
     return( 
       <>
       <form onSubmit={this.handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>Steam Vanity URL</Form.Label>
+      {/* <Form.Control type="text" placeholder="Enter your Steam vanity URL" onChange={this.onInput} /> */}
+      <Form.Control type="text" placeholder="Enter your Steam vanity URL" name="steamId" />
+      {/* <Form.Text className="text-muted">
+        Insert cool things to say here
+      </Form.Text> */}
+    </Form.Group>
         <Button type='submit'>Nice button!</Button>
       </form>
       </>
